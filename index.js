794f848015706fefc702dc0901ed284fdb0473e1
mikaelec <mikaelec@gmail.com>
2016-09-07 21:28:30 +0200

    REFACTOR!

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
 let app = require('express')();
-var request = require('sync-request');
+let request = require('sync-request');
 let ical = require('ical.js');
 
 app.get('/', (req, res) => {
-    let userid = req.query.userid;
-    let authtoken = req.query.authtoken;
+    try {
+        let userid = req.query.userid;
+        let authtoken = req.query.authtoken;
+        if (!/[0-9]{1,100}/.test(userid) || !/[0-9a-z]{1,100}/.test(authtoken))
+        {
+            res.statusCode = 403;
+            res.send();
+            return;
+        }
 
-    if (!/[0-9]{5}/.test(userid) || !/[0-9a-z]{30,50}/.test(authtoken))
-    {
-        res.statusCode = 403;
+        let ical = parseCalendar(userid, authtoken);
+        res.header('content-type', 'text/calendar; charset=utf-8');
+        res.header('content-disposition', 'attachment; filename=icalexport.ics');
+        res.send(ical);
+        return;
+    } catch (error) {
+        res.statusCode = 500;
         res.send();
         return;
     }
-
-    let ical = parseCalendar(userid, authtoken);
-
-    res.header('content-type', 'text/calendar; charset=utf-8');
-    res.header('content-disposition', 'attachment; filename=icalexport.ics');
-    res.send(ical);
 });
 
 app.listen(process.env.PORT || 5000);
 
 function parseCalendar(userid, authtoken) {
-    var res = request('GET', `https://www.moodle.aau.dk/calendar/export_execute.php?userid=${userid}&authtoken=${authtoken}&preset_what=all&preset_time=recentupcoming`);
-    var cal = res.getBody().toString();
+    let url = `https://www.moodle.aau.dk/calendar/export_execute.php?userid=${userid}&authtoken=${authtoken}&preset_what=all&preset_time=recentupcoming`;
+    let rawCal = request('GET', url).getBody().toString();
+    let component = new ical.Component(ical.parse(rawCal));
+    let events = component.getAllSubcomponents('vevent');
 
-    let jcal = ical.parse(cal);
-    let comp = new ical.Component(jcal);
-    let events = comp.getAllSubcomponents('vevent');
-
-    events.forEach((event, i) => {
+    events.forEach(event => {
         let _event = new ical.Event(event);
         _event.description = _event.summary;
 
-        let summaryRegex = /^((?:(?:Course|Kursus): (.*?))?(?: - Note: (.*?))?(?: - Time: (.*?))?(?: - Place: (.*?))?(?: - Teacher: (.*))?)$/;
-        let abrvRegex = /.*?(?:\(([^\s]*?)\)).*/;
-        let match = _event.summary.match(summaryRegex);
-
+        let match = _event.summary.match(/^((?:(?:Course|Kursus): (.*?))?(?: - Note: (.*?))?(?: - Time: (.*?))?(?: - Place: (.*?))?(?: - Teacher: (.*))?)$/);
         if (match) {
             if (match[2]) {
-                let abrv = match[2].match(abrvRegex);
+                let abrv = match[2].match(/.*?(?:\(([^\s]*?)\)).*/);
                 _event.summary = abrv ? abrv[1] : match[2];
             }
             if (match[5])
@@ -48,5 +48,5 @@ function parseCalendar(userid, authtoken) {
         }
     });
 
-    return comp.toString();
+    return component.toString();
 }
